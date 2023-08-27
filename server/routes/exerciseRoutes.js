5b8234159d03f68f1b97022b59a0b0c8da486e40
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-08-27 18:00:55 +0200

    add protected routes CRUD

--- server/routes/exerciseRoutes.js
+++ server/routes/exerciseRoutes.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const authMiddleWare = require('../middleware/authMiddleWare'); // Import authMiddleWare
+const authMiddleWare = require('../middleware/authMiddleWare');
+const exerciseController = require('../controllers/exerciseController'); // Import du contrôleur spécifique aux exercices
 
 // Routes d'authentification :
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
-// Utilisation du middleware pour protéger les routes suivantes
-router.get('/protected-route', authMiddleWare, (req, res) => {
-    // Votre code ici
-});
+// Routes protégées par le middleware d'authentification : 
+// Routes CRUD pour les exercices
+router.post('/exercises', authMiddleWare, exerciseController.createExercise);
+router.get('/exercises', authMiddleWare, exerciseController.getAllExercises);
+router.get('/exercises/:id', authMiddleWare, exerciseController.getExerciseById);
+router.put('/exercises/:id', authMiddleWare, exerciseController.updateExercise);
+router.delete('/exercises/:id', authMiddleWare, exerciseController.deleteExercise);
 
 module.exports = router;
