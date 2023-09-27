624736cd55f13149bd97dc8c281ecd1cba0ebc0d
Æscanor <113015001+Aescanor@users.noreply.github.com>
2023-09-27 20:26:40 +0200

    add manage cookies in authMiddleWare et checkAuthMiddleWare

--- server/middleware/checkAuthMiddleWare.js
+++ server/middleware/checkAuthMiddleWare.js
@@ -1,28 +1,27 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const checkAuth = (req, res, next) => {
-
     try {
-        // Récupération du token dans le header de la requête : 
-        const token = req.cookies.token;
+        // Utilisation de cookie-parser pour analyser les cookies
+        cookieParser()(req, res, () => {
+            const token = req.cookies.token;
 
-        // Vérification du token :
-        if(!token) {
-            return res.status(401).json({ message: "Vous devez vous connecter pour accéder à cette ressource" });
-        }
-        else {
-            console.log(token, "vous êtes authentifié");
-        }
+            if (!token) {
+                return res.status(401).json({ message: "Vous devez vous connecter pour accéder à cette ressource" });
+            } else {
+                console.log(token, "vous êtes authentifié");
+            }
 
-        // Décodage du token :
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
-        //Ajouter les données du token décodé à la requête :        
-        req.userData = decodedToken;
-        next();
-    }catch (error) {
-        res.status(401).json({ message: "Session expiré, veuillez vous reconnecter" });
+            // Ajouter les données du token décodé à la requête :
+            req.userData = decodedToken;
+            next();
+        });
+    } catch (error) {
+        res.status(401).json({ message: "Session expirée, veuillez vous reconnecter" });
     }
-}
+};
 
 module.exports = checkAuth;
